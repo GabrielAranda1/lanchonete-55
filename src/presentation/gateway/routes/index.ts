919b6b8942ed422d0718c0a75aedefd33116fce3
GabrielAranda1 <per.gabrielaranda@gmail.com>
2023-07-07 19:44:11 -0300

    doc: created /doc endpoint to serve swagger

--- src/presentation/gateway/routes/index.ts
+++ src/presentation/gateway/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { customerRoutes } from './CustomerRoutes'
 import { orderRoutes } from './OrderRoutes'
 import { productRoutes } from './ProductRoutes'
+import { utilRoutes } from './UtilsRoutes'
 
 const routes = Router()
 
 routes.use(customerRoutes)
 routes.use(productRoutes)
 routes.use(orderRoutes)
+routes.use(utilRoutes)
 
 export { routes }
