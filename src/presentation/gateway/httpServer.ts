1f50f16a5e1cfc209af668895f34e4dc9d3285ab
GabrielAranda1 <per.gabrielaranda@gmail.com>
2023-09-03 16:34:42 -0300

    refactor: aded http server port by env

--- src/presentation/gateway/httpServer.ts
+++ src/presentation/gateway/httpServer.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { container } from 'tsyringe';
-import { adaptErrorHandler } from '../adapters/ExpressErrorHandlerAdapter';
-import { HttpErrorHandler } from './middlewares/ErrorHandler';
-import { routes } from './routes';
+import { container } from 'tsyringe'
+import { adaptErrorHandler } from '../adapters/ExpressErrorHandlerAdapter'
+import { HttpErrorHandler } from './middlewares/ErrorHandler'
+import { routes } from './routes'
 
 function startHttpServer() {
 
@@ -10,19 +10,19 @@ function startHttpServer() {
 
   const server = express()
 
-  server.use(express.json());
-  server.use(express.urlencoded({ extended: true }));
+  server.use(express.json())
+  server.use(express.urlencoded({ extended: true }))
   server.use(routes)
 
   server.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     const response = errorHandler.handle(err)
-    const errorHandlerAdapter = adaptErrorHandler(response);
+    const errorHandlerAdapter = adaptErrorHandler(response)
     return errorHandlerAdapter(req, res, next)
   })
 
-  server.listen(3000, () => {
-    console.log('Http server running at port 3000');
-  });
+  server.listen(process.env.PORT || 3000, () => {
+    console.log(`Http server running at port ${process.env.PORT}`)
+  })
 }
 
 export { startHttpServer }
