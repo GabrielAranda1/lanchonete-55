4adebd15bc3f0aeab3da8d97ce9806640d5c8ac0
Rafael Nunes Vazquez <rafaelnunesvazquez@gmail.com>
2023-09-03 17:54:03 -0300

    chore: added update order status based on payment status

--- src/domain/usecases/UpdatePaymentStatus/UpdatePaymentStatus.ts
+++ src/domain/usecases/UpdatePaymentStatus/UpdatePaymentStatus.ts
@@ -5,18 +5,22 @@ import { InvalidParamError } from "../../errors/InvalidParam";
 import { IPaymentRepository } from "../../ports/repositories/Payment";
 import { IUpdatePaymentStatusUseCase } from "./IUpdatePaymentStatus";
 import { Payment, Status } from "../../entities/Payment";
+import * as Order from "../../entities/Order";
+import { IOrderRepository } from "../../ports/repositories/Order";
 
 @injectable()
 export class UpdatePaymentStatusUseCase implements IUpdatePaymentStatusUseCase {
     constructor(
         @inject('IPaymentRepository')
-        private readonly paymentRepository: IPaymentRepository
+        private readonly paymentRepository: IPaymentRepository,
+        @inject('IOrderRepository')
+        private readonly orderRepository: IOrderRepository
     ) { }
 
     async update(params: UpdatePaymentStatusDTO): Promise<Payment> {
         const {id, status} = params
 
-        const isValidStatus = Object.values(Status).includes(status as Status)
+        const isValidStatus = [Status.PAID, Status.NOTPAID].includes(status)
 
         if (!isValidStatus) throw new InvalidParamError('Invalid param: status')
 
@@ -28,6 +32,21 @@ export class UpdatePaymentStatusUseCase implements IUpdatePaymentStatusUseCase {
 
         if (!payment) throw new NotFoundError('Payment not found')
 
+        await this.updateOrderStatus(status, payment.orderId)
+
         return payment;
+    }
+
+    private async updateOrderStatus(status: Status, orderId: string){
+        const order = this.orderRepository.getById(orderId)
+
+        if(!order) throw new NotFoundError('Order Payment not found')
+
+        if(status === Status.PAID){
+            this.orderRepository.updateStatus(orderId, Order.Status.SUCCESSFULPAYMENT)
+        }
+        else if (status === Status.NOTPAID){
+            this.orderRepository.updateStatus(orderId, Order.Status.PAYMENTPROBLEM)
+        }
     }
 }
